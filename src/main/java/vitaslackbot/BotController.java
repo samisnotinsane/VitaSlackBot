4893be29e243a27198d83e4acf970710f7c64dff
starfighter-ix <ec14370@qmul.ac.uk>
2017-03-25 03:14:00 +0000

    Added conversation for setting up meeting.

--- src/main/java/vitaslackbot/BotController.java
+++ src/main/java/vitaslackbot/BotController.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.util.regex.Matcher;
+
 /**
  * Created by sameen on 25/03/2017.
  */
@@ -48,8 +50,79 @@ public class BotController extends Bot {
      */
     @Controller(events = {EventType.DIRECT_MENTION, EventType.DIRECT_MESSAGE})
     public void onReceiveDM(WebSocketSession session, Event event) {
-        reply(session, event, new Message("Hi, this is " + slackService.getCurrentUser().getName()));
+
+        String userText = event.getText();
+        logger.info("BotController", "Received from user: " + userText);
+        if(userText.contains("hi")) {
+            reply(session, event, new Message("Hey!"));
+            logger.info("BotController", "Replied to user: Hey!");
+        } else if (userText.contains("hey")) {
+            reply(session, event, new Message("Hi, this is " + slackService.getCurrentUser().getName()));
+        } else if (userText.contains("arrange new meeting")) {
+            startConversation(event, "setupMeeting");
+            logger.info("BotController", "started conversation: setupMeeting");
+        }
     }
 
+    /**
+     * Conversation feature of JBot. This method is the starting point of the conversation (as it
+     * calls {@link Bot#startConversation(Event, String)} within it. You can chain methods which will be invoked
+     * one after the other leading to a conversation. You can chain methods with {@link Controller#next()} by
+     * specifying the method name to chain with.
+     *
+     * @param session
+     * @param event
+     */
+    @Controller(pattern = "(setup meeting)", next = "confirmTiming")
+    public void setupMeeting(WebSocketSession session, Event event) {
+        startConversation(event, "confirmTiming");   // start conversation
+        reply(session, event, new Message("Cool! At what time (ex. 15:30) do you want me to set up the meeting?"));
+    }
+
+    /**
+     * This method is chained with {@link BotController#setupMeeting(WebSocketSession, Event)}.
+     *
+     * @param session
+     * @param event
+     */
+    @Controller(next = "askTimeForMeeting")
+    public void confirmTiming(WebSocketSession session, Event event) {
+        reply(session, event, new Message("Your meeting is set at " + event.getText() +
+                ". Would you like to repeat it tomorrow?"));
+        nextConversation(event);    // jump to next question in conversation
+    }
+
+    /**
+     * This method is chained with {@link BotController#confirmTiming(WebSocketSession, Event)}.
+     *
+     * @param session
+     * @param event
+     */
+    @Controller(next = "askWhetherToRepeat")
+    public void askTimeForMeeting(WebSocketSession session, Event event) {
+        if (event.getText().contains("yes")) {
+            reply(session, event, new Message("Okay. Would you like me to set a reminder for you?"));
+            nextConversation(event);    // jump to next question in conversation
+        } else {
+            reply(session, event, new Message("No problem. You can always schedule one with 'setup meeting' command."));
+            stopConversation(event);    // stop conversation only if user says no
+        }
+    }
+
+    /**
+     * This method is chained with {@link BotController#askTimeForMeeting(WebSocketSession, Event)}.
+     *
+     * @param session
+     * @param event
+     */
+    @Controller
+    public void askWhetherToRepeat(WebSocketSession session, Event event) {
+        if (event.getText().contains("yes")) {
+            reply(session, event, new Message("Great! I will remind you tomorrow before the meeting."));
+        } else {
+            reply(session, event, new Message("Oh! my boss is smart enough to remind himself :)"));
+        }
+        stopConversation(event);    // stop conversation
+    }
 
 }
